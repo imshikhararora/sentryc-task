0b6736fa40fed5211be4385f600014703fb5d421
Shikhar Arora <hi.shikhararora@gmail.com>
2024-03-13 12:21:02 +0100

    initial commit

---
+++ src/main/java/com/sentryc/sentryctask/SentrycTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sentryc.sentryctask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentrycTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentrycTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sentryc/sentryctask/SentrycTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentryc.sentryctask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentrycTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
